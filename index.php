ffea30a562f752910ced6c52bdab7057b66efd1f
LUBIA SILVEIRA DE BARROS <00457356200@ifro.local>
2016-05-06 11:35:32 -0400

    pagina inicial

--- index.php
+++ index.php
@@ -7,11 +7,42 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Portal Kardashians </title>
+        <style>
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                font-family: "Arial", sans-serif;
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            li a:hover {
+                background-color: #111;
+            }
+        </style>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="/Apresentacao/equipe.php">Apresentação</a></li>
+            <li><a href="/Contatos/Passo5.html">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+
     </body>
 </html>
